893610cefc7b2acf5489f9b91cd8d015c0b47008
Natalie <104013228+NataJgerenaia@users.noreply.github.com>
2026-02-11 17:37:22 +0400

    api tests

--- src/test/java/API_tests/APItest.java
+++ src/test/java/API_tests/APItest.java
@@ -1,4 +1,52 @@
 package API_tests;
 
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
 public class APItest {
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://automationexercise.com/api";
+    }
+
+    @Test
+    public void testGetAllProductsList_API1() {
+        Response response = get("/productsList");
+        Assert.assertEquals(response.getStatusCode(), 200);
+        Assert.assertTrue(response.getBody().asString().contains("products"), "Body should contain products");
+    }
+
+    @Test
+    public void testPostToAllProductsList_API2() {
+        Response response = post("/productsList");
+        JsonPath json = response.jsonPath();
+
+        Assert.assertEquals(json.get("responseCode"), (Integer) 405);
+        // FIXED: Added a dot at the end of the string
+        Assert.assertEquals(json.get("message"), "This request method is not supported.");
+    }
+
+    @Test
+    public void testGetAllBrandsList_API3() {
+        Response response = get("/brandsList");
+        Assert.assertEquals(response.getStatusCode(), 200);
+        Assert.assertTrue(response.getBody().asString().contains("brands"), "Body should contain brands");
+    }
+
+    @Test
+    public void testPutToAllBrandsList_API4() {
+        Response response = put("/brandsList");
+        JsonPath json = response.jsonPath();
+
+        Assert.assertEquals(json.get("responseCode"), (Integer) 405);
+        // FIXED: Added a dot at the end of the string
+        Assert.assertEquals(json.get("message"), "This request method is not supported.");
+    }
 }
